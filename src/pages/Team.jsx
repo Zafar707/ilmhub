099aedeff35c29053b9471108f069c8605d9a576
Zafar707 <devloper707@gmail.com>
2025-06-30 22:21:19 +0500

    Remove missing FAQSection import from Team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import TeamIntroSection from "../components/team/TeamIntroSection.jsx";
 import TeamMembersSection from "../components/team/TeamMembersSection.jsx";
 import CTASection from "../components/about/ContactSection.jsx"; // Re-use CTA
-import FAQSection from "../components/about/FAQSection.jsx";
 
 
 const Team = () => {
@@ -11,7 +10,6 @@ const Team = () => {
     <main className="mt-30">
       <TeamIntroSection />
       <TeamMembersSection />
-      <FAQSection />
       <CTASection />
     </main>
   );
