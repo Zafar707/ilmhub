1cb478efb794a99c133b0dd645c80daf5732f084
Zafar707 <devloper707@gmail.com>
2025-06-30 22:15:43 +0500

    a

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import TeamIntroSection from "../components/team/TeamIntroSection.jsx";
 import TeamMembersSection from "../components/team/TeamMembersSection.jsx";
 import CTASection from "../components/about/ContactSection.jsx"; // Re-use CTA
-import FaqSection from "../components/about/FAQSection.jsx";
+import FAQSection from "../components/about/FAQSection.jsx";
+
 
 const Team = () => {
   return (
     <main className="mt-30">
       <TeamIntroSection />
       <TeamMembersSection />
-      <FaqSection />
+      <FAQSection />
       <CTASection />
     </main>
   );
